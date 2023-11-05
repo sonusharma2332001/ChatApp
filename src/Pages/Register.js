d78d050cca42d45a8de4e473aadaad27458df1cf
sonusharma2332001 <75514465+sonusharma2332001@users.noreply.github.com>
2023-11-05 10:28:06 +0530

    Update Register.js

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -55,7 +55,7 @@ const Register = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Lama Chat</span>
+        <span className="logo">Oum</span>
         <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
           <input required type="text" placeholder="display name" />
